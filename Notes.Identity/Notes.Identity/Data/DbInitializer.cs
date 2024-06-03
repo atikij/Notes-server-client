86c27d64b4cb32e78e8bb2ab906c325919edc27d
Niki <niki@gmail.com>
2024-06-04 02:24:31 +0300

    1

--- Notes.Identity/Notes.Identity/Data/DbInitializer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Notes.Identity.Data;
-
-public class DbInitializer
-{
-    public static void Initialize(AuthDbContext context)
-    {
-        context.Database.EnsureCreated();
-    }
-}
